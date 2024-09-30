01f3405f8476e4076baa1cfe1c727faf7ef9f638
NOTE loja <matheus@gmail.com>
2024-09-30 17:28:54 -0300

    cors config

--- spring/loja/src/main/java/com/br/maskotes/loja/security/WebSecurityConfig.java
+++ spring/loja/src/main/java/com/br/maskotes/loja/security/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public WebMvcConfigurer corsConfig(){
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:8080" , "http://192.168.1.229:8080")
                         .allowedMethods(HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.DELETE.name(),
